7382bbdfef8cf9ff89b2c4899e539e77f6783d0f
flar2 <asegaert@gmail.com>
2015-05-18 00:23:09 -0400

    sched: don't let pnpmgr drop budget below MIN_BUDGET
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -47,6 +47,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
+#define MIN_BUDGET	40
+
 unsigned int sysctl_sched_latency = 6000000ULL;
 unsigned int normalized_sysctl_sched_latency = 6000000ULL;
 
@@ -2532,7 +2534,10 @@ int sched_set_cpu_budget(int cpu, int budget)
 {
 	struct rq *rq = cpu_rq(cpu);
 
-	rq->budget = budget;
+	if (cpu < 6)
+		rq->budget = max(budget, MIN_BUDGET);
+	else
+		rq->budget = budget;
 
 	return 0;
 }
