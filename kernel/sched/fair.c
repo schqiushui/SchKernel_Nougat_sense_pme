7852cde81100409a213d65afe291c6f116aac452
flar2 <asegaert@gmail.com>
2016-06-01 10:14:20 -0400

    sched: change default min_budget to 60

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -48,7 +48,7 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-static int min_budget = 80;
+static int min_budget = 60;
 module_param(min_budget, int, 0755);
 
 unsigned int sysctl_sched_latency = 6000000ULL;
